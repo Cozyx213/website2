fae308eccdbe5cc212ea889c5320a7deec35a296
Jake <bajentingjackie@gmail.com>
2023-10-14 11:10:01 +0800

    changed home image and deleted useless routes

--- app.py
+++ app.py
@@ -25,18 +25,15 @@ def download_ppt(ppt_filename):
 @app.route('/')
 def endix():
     return render_template("endix.html")
-@app.route("/Chemistry2")
-def Chemistry_2():
-    return render_template('Chemistry2.html')
-@app.route("/Chemistry3")
-def Chemistry_3():
-    return render_template('Chemistry3.html')
 @app.route("/Form")
 def Form():
     return render_template("Form.html", strands=phrases.STRANDS(), sections=phrases.SECTIONS())
 @app.route("/History")
 def history():
     return render_template('History.html')
+@app.route("/Create")
+def Create():
+    return render_template('Create.html')
 @app.route("/Campus_Map")
 def Map():
     return render_template('Campus_Map.html')
